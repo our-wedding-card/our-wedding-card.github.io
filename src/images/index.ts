be6507beb87b7261579d0d3db04edf104de637b5
our-wedding-card <gjeong.park@gmail.com>
2026-07-26 22:10:43 +0900

    Update index.ts

--- src/images/index.ts
+++ src/images/index.ts
@@ -4,6 +4,17 @@ import gallery2 from "./gallery2.jpg"
 import gallery3 from "./gallery3.jpg"
 import gallery4 from "./gallery4.jpg"
 import gallery5 from "./gallery5.jpg"
+import gallery6 from "./gallery6.jpg"
+import gallery7 from "./gallery7.jpg"
+import gallery8 from "./gallery8.jpg"
+import gallery9 from "./gallery9.jpg"
+import gallery10 from "./gallery10.jpg"
+import gallery11 from "./gallery11.jpg"
+import gallery12 from "./gallery12.jpg"
+import gallery13 from "./gallery13.jpg"
+import gallery14 from "./gallery14.jpg"
+import gallery15 from "./gallery15.jpg"
+
 
 /**
  * 메인 커버 이미지
@@ -19,4 +30,14 @@ export const GALLERY_IMAGES = [
   gallery3,
   gallery4,
   gallery5,
+  gallery6,
+  gallery7,
+  gallery8,
+  gallery9,
+  gallery10,
+  gallery11,
+  gallery12,
+  gallery13,
+  gallery14,
+  gallery15,
 ]
