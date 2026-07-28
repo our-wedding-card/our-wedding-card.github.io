05a4b0b85e7a1906485141f96c20d66298a72ae3
our-wedding-card <gjeong.park@gmail.com>
2026-07-28 16:06:42 +0900

    Update index.tsx

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -16,9 +16,9 @@ export const Information1 = () => {
       <div className="info-card">
         <div className="label">식사 안내</div>
         <div className="content">
-          식사시간: 12시 30분 ~ 14시 30분
+          식사시간: 12시 ~ 14시 30분
           <br />
-          장소: 지하 1층 연회장
+          장소: 1층 연회장(오키드홀)
         </div>
       </div>
     </>
